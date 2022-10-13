4a36ccad56147ed5801d0318317f5d88ef1b8210
nguyenngoclannhu <nguyenngoclannhu@gmail.com>
2022-10-13 20:09:52 +0700

    add timer

--- PTPY2/M2L2-4/RuffierTest/BK_main.py
+++ PTPY2/M2L2-4/RuffierTest/BK_main.py
@@ -4,7 +4,7 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-
+from kivy.clock import Clock
 from ruffier import *
 
 name = ""
@@ -82,21 +82,40 @@ class Screen2(Screen):
 class Screen3(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
+        self.next_screen = False 
         label1 = Label(text="Do squat in 45 seconds")
-
+        
+        self.lbl_sec = Label(text = "Seconds elapsed: ")
+        self.time = 0
         self.btn = Button(text='Next', size_hint=(0.2, None),
                           height='30sp', pos_hint={'center_x': 0.5})
         
         self.btn.on_press = self.next
         self.main_layout = BoxLayout(orientation = 'vertical')
+        self.main_layout.add_widget(self.lbl_sec)
         self.main_layout.add_widget(label1)
         self.main_layout.add_widget(self.btn)
 
         self.add_widget(self.main_layout)
+    
+    def on_enter(self):
+        Clock.schedule_once(self.finished, 10)
+        self.event = Clock.schedule_interval(self.display_time, 1)
+    
+    def finished(self, dt):
+        self.next_screen = True
+        self.btn.set_disabled(False)
+        self.event.cancel()
+
+    def display_time(self, dt):
+        self.time += 1
+        self.lbl_sec.text = "Second elapsed: " + str(self.time)
 
     def next(self):
-        self.manager.current = 'fourth'
+        if not self.next_screen:
+            self.btn.set_disabled(True)
+        else:
+            self.manager.current = 'fourth'
 
 class Screen4(Screen):
     def __init__(self, **kwargs):
@@ -109,6 +128,9 @@ class Screen4(Screen):
 
         inp2_lb = Label(text="Enter your pulse")
         self.inp2 = TextInput(multiline=False)
+
+        self.lbl_sec = Label("Seconds elapsed: ")
+        self.time = 1
 
         self.main_layout = BoxLayout(orientation='vertical')
         line1 = BoxLayout(size_hint=(0.8, None), height='30sp')
@@ -129,13 +151,27 @@ class Screen4(Screen):
         self.main_layout.add_widget(self.btn)
 
         self.add_widget(self.main_layout)
+    
+    def on_enter(self):
+        Clock.schedule_once(self.finished, 5)
+        self.event = Clock.schedule_interval(self.display_time, 1)
+    
+    def display_time(self, dt):
+        self.time += 1
+        self.lbl_sec.text = "Seconds elasped: " + str(self.time)
+
 
     def next(self):
-        self.manager.current = "fifth"
+        if not self.next_screen:
+            self.btn.set_disabled(True)
+            self.inp1.set_disabled(True)
+            self.inp2.set_disabled(True)
+        else:
+            self.manager.current = "fifth"
 
-        global p2, p3 
-        p2 = int(self.inp1.text)
-        p3 = int(self.inp2.text)
+            global p2, p3 
+            p2 = int(self.inp1.text)
+            p3 = int(self.inp2.text)
 
 class Screen5(Screen):
     def __init__(self, **kwargs):
